8d0406d5e5556593fe6c64641a3daf7b9b84996d
annelaure_freant <53858673+annelaure-blog@users.noreply.github.com>
2025-10-08 15:06:19 +0200

    Fix bedouins label dimensions

--- src/scripts/layout.js
+++ src/scripts/layout.js
@@ -38,10 +38,6 @@ const sceneLayout = {
     },
     bedouins: {
       position: { default: { x: 0.32, y: 0.6 }, mobile: { x: 0.35, y: 0.62 } },
-      size: {
-        width: '352px',
-        height: '74px',
-      },
       fontScale: { default: 1.5, mobile: 1.35 },
       layer: 2,
     },
